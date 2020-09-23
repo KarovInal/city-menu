e3d765f63cbb9480bd6979bc2a6ee9e4ed9b9575
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-09-23 08:33:27 +0300

    feat: Added `mode` to a theme

--- src/components/typography/header.js
+++ src/components/typography/header.js
@@ -3,7 +3,7 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    color: theme.main.primaryTextColor,
+    color: theme.mode.primary.primaryTextColor,
     /* Header */
     fontFamily: "Roboto",
     fontStyle: "normal",
--- src/constants/theme.js
+++ src/constants/theme.js
@@ -11,31 +11,71 @@ export const colors = {
 
 export const logo = 'https://firebasestorage.googleapis.com/v0/b/molvee-e32fd.appspot.com/o/Logo.png?alt=media&token=a4a6579b-80f6-48b0-bde3-40ea76793b46';
 
-export const themeConfig = {
-  overrides: {
-    MuiButton: {
-      root: {
-        borderRadius: '8px',
+export const createThemeConfig = (mode) => {
+  const theme = {
+    overrides: {
+      MuiButton: {
+        root: {
+          borderRadius: '8px',
+        },
+      },
+      MuiAppBar: {
+        root: {
+          boxShadow: 'none'
+        },
+      },
+      MuiToolbar: {
+        root: {
+          color: colors.primaryTextColor,
+          backgroundColor: colors.primaryBackgroundColor,
+        },
+      }
+    },
+    palette: {
+      primary: {
+        main: colors.primaryColor
+      },
+      secondary: {
+        main: colors.secondaryColor
+      }
+    },
+    light: {
+      primary: {
+        primaryColor: '#ec1c24',
+        primaryTextColor: '#1a1a1a',
+        primaryBackgroundColor: '#fff',
+        disabledTextColor: '#949494',
+        borderColor: '#e6e6e6',
+      },
+      secondary: {
+        secondaryColor: '#231f20',
+        secondaryTextColor: '#616161',
+        secondaryBackgroundColor: '#231f20',
+        disabledTextColor: '#949494',
+        borderColor: '#e6e6e6',
       },
     },
-    MuiAppBar: {
-      root: {
-        boxShadow: 'none'
+    // TODO [NZ] 23.09.2020: Define dark theme colors, now same as 'light theme'
+    dark: {
+      primary: {
+        primaryColor: '#ec1c24',
+        primaryTextColor: '#1a1a1a',
+        primaryBackgroundColor: '#fff',
+        disabledTextColor: '#949494',
+        borderColor: '#e6e6e6',
+      },
+      secondary: {
+        secondaryColor: '#231f20',
+        secondaryTextColor: '#616161',
+        secondaryBackgroundColor: '#231f20',
+        disabledTextColor: '#949494',
+        borderColor: '#e6e6e6',
       },
     },
-    MuiToolbar: {
-      root: {
-        color: colors.primaryTextColor,
-        backgroundColor: colors.primaryBackgroundColor,
-      },
-    }
-  },
-  palette: {
-    primary: {
-      main: colors.primaryColor
-    },
-    secondary: {
-      main: colors.secondaryColor
-    }
-  }
-}
+  };
+
+  return {
+    ...theme,
+    mode: theme[mode],
+  };
+};
