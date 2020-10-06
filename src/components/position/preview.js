5092cc1da1d134eaa536cf40105a130592b03d09
i.karov <i.karov@aliexpress.ru>
2020-10-06 21:07:13 +0300

    Fix preview component

--- src/components/position/preview.js
+++ src/components/position/preview.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
-const useStyles = size => makeStyles({
+const useStyles = (size, preview) => makeStyles({
   root: {
     height: size === 'sm' ? '80px' : '140px',
-    weight: size === 'sm' ? '80px' : '140px',
+    width: size === 'sm' ? '80px' : '140px',
     border: '0.5px solid rgba(0, 0, 0, 0.08)',
     boxSizing: 'border-box',
     borderRadius: '12px',
+    flexShrink: 0,
+    backgroundImage: `url('${preview}')`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
   },
 });
 
 export const Preview = React.memo(({ preview, size = 'md' }) => {
-  const classes = useStyles(size)();
+  const classes = useStyles(size, preview)();
 
   return (
-    <img className={classes.root} src={preview} alt="position-preview" draggable="false" />
+    <div className={classes.root} />
   );
 });
