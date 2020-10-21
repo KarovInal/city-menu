1f05c18c069fde30499c414b2a6fa9cd022dbc01
Zhumabaev <nadir.zhumabaev@yandex.ru>
2020-10-22 01:20:55 +0300

    feat: Added proceed button shaking & total price show

--- src/components/proceed-button.js
+++ src/components/proceed-button.js
@@ -1,26 +1,66 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import cn from 'classnames';
 import { Title } from "./typography/title";
 import { Body2 } from "./typography/body2";
 import { Flex } from "./flex";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Button from "@material-ui/core/Button";
+import { useSelector } from "react-redux";
+import { getPriceSelector } from "../selectors/dishes-selector";
 
 const useStyles = makeStyles({
   root: {
     color: 'white',
   },
+  w90p: {
+    width: '90%',
+  },
+  w100p: {
+    width: '100%',
+  },
+  sticky: {
+    position: 'fixed',
+    bottom: '10px',
+    width: 'calc(100% - 24px)',
+    margin: '0px 12px',
+    boxSizing: 'border-box',
+  },
 });
 
-export const ProceedButton = React.memo(({ totalPrice }) => {
+export const ProceedButton = React.memo(() => {
+  const [totalPrice] = useSelector(getPriceSelector)(true);
+
+  const [animateClass, setAnimateClass] = React.useState('');
+
+  const setClass = React.useCallback(() => {
+    setAnimateClass("animate__animated animate__headShake");
+
+    setTimeout(() => setAnimateClass(''), 500);
+  })
+
+  React.useEffect(() => {
+    if (!totalPrice) {
+      return;
+    }
+
+    setClass();
+  }, [totalPrice])
+
   const classes = useStyles();
   const history = useHistory();
   const goToCartPage = () => history.push('/cart');
 
   return (
-    <Button disableElevation color='secondary' variant="contained" onClick={goToCartPage}>
-      <Flex justifyContent="space-between">
-        <Title className={classes.root}>{totalPrice}</Title>
+    <Button
+      disableElevation
+      color='secondary'
+      variant='contained'
+      onClick={goToCartPage}
+      className={cn(classes.w90p, classes.sticky, animateClass)}
+    >
+      <Flex className={classes.w100p} justifyContent="space-between" alignItems="center">
+        <Title className={classes.root}>{totalPrice} ₽</Title>
         <Body2 className={classes.root}>ПЕРЕЙТИ В КОРЗИНУ</Body2>
       </Flex>
     </Button>
