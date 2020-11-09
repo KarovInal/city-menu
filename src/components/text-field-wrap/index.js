daf6f0e403734ae6124ea4a75933b6cde9d74707
i.karov <i.karov@aliexpress.ru>
2020-11-09 17:54:07 +0300

    Added form components

---
+++ src/components/text-field-wrap/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import InputMask from 'react-input-mask';
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import { Controller } from "react-hook-form";
+import { Grid } from "@material-ui/core";
+
+export const TextFieldWrap = ({ touched = {}, errors = {}, name = '', ...otherProps }) => {
+  const isError = !!errors[name];
+  const isTouched = !!touched[name];
+
+  return (
+    <Grid container>
+      <TextField color='secondary' variant='outlined' error={isTouched && isError} name={name} {...otherProps} />
+      {
+        (isTouched && isError) && (
+          <FormHelperText error>Это поле обязательно для заполнения</FormHelperText>
+        )
+      }
+    </Grid>
+  )
+}
+
+export const TextFieldWithMaskWrap = ({ control, name, alwaysShowMask, mask, touched, errors, fullWidth, rules, type }) => {
+  return (
+    <Controller
+      rules={rules}
+      name={name}
+      defaultValue=''
+      color='secondary'
+      control={control}
+      render={({ name, onBlur, onChange, ref }) => {
+        return (
+          <InputMask
+            ref={ref}
+            mask={mask}
+            name={name}
+            onBlur={onBlur}
+            onChange={onChange}
+            alwaysShowMask={alwaysShowMask}
+          >
+            {() => <TextFieldWrap name={name} touched={touched} errors={errors} fullWidth={fullWidth} type={type} />}
+          </InputMask>
+        )
+      }}
+    />
+  )
+}
